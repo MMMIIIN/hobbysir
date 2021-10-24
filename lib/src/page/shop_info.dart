138a4c4402dabff5493c59966eb8ff432ffda022
MMMIIIN <wbsldj7645@naver.com>
2021-10-24 20:51:47 +0900

    shopInfo update

--- lib/src/page/shop_info.dart
+++ lib/src/page/shop_info.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'package:dwaf_hobbysir/src/shop.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:get/get.dart';
 
 class ShopInfoPage extends StatefulWidget {
   int index;
@@ -24,20 +25,129 @@ class _ShopInfoPageState extends State<ShopInfoPage> {
     ];
     return Material(
       child: SafeArea(
-        child: Container(
-          child: GoogleMap(
-            mapType: MapType.normal,
-            initialCameraPosition: CameraPosition(
-              target: testShopList[widget.index].latLng,
-              zoom: 14.4746,
+        child: Column(
+          children: [
+            Container(
+              height: context.mediaQuery.size.height * 0.05,
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Home',
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                  Icon(
+                    Icons.favorite_border,
+                    color: Colors.red,
+                  )
+                ],
+              ),
             ),
-            onMapCreated: (GoogleMapController controller) {
-              _controller.complete(controller);
-            },
-            markers: markers.toSet(),
-          ),
+            Container(
+              height: context.mediaQuery.size.height * 0.7,
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Container(
+                      height: context.mediaQuery.size.height * 0.4,
+                      child: GoogleMap(
+                        mapType: MapType.normal,
+                        initialCameraPosition: CameraPosition(
+                          target: testShopList[widget.index].latLng,
+                          zoom: 14.4746,
+                        ),
+                        onMapCreated: (GoogleMapController controller) {
+                          _controller.complete(controller);
+                        },
+                        markers: markers.toSet(),
+                      ),
+                    ),
+                    locationWidget(),
+                    textWidget(),
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
+  }
+
+  Widget locationWidget() {
+    return Container(
+      height: context.mediaQuery.size.height * 0.1,
+      padding: EdgeInsets.symmetric(vertical: 10),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.gps_fixed,
+                color: Colors.blue,
+                size: 20,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 10),
+                child: Text(
+                  testShopList[widget.index].address,
+                  style: TextStyle(
+                    color: Colors.black54,
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+              Icon(
+                Icons.paste,
+                color: Colors.grey,
+                size: 20,
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Row(
+                children: [
+                  Icon(
+                    Icons.directions_bus_rounded,
+                    color: Colors.blue,
+                    size: 20,
+                  ),
+                  Text(
+                    '452m',
+                    style: TextStyle(color: Colors.black54),
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  Icon(
+                    Icons.local_parking,
+                    color: Colors.blue,
+                    size: 20,
+                  ),
+                  Text(
+                    testShopList[widget.index].isParking ? '주차가능' : '주차불가',
+                    style: TextStyle(
+                      color: Colors.black54,
+                      fontSize: 16,
+                    ),
+                  )
+                ],
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget textWidget() {
+    return Text('abc');
   }
 }
--- lib/src/shop.dart
+++ lib/src/shop.dart
@@ -2,18 +2,24 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class ShopInfo {
   String title;
+  String address;
+  bool isParking;
   LatLng latLng;
 
-  ShopInfo({required this.title, required this.latLng});
+  ShopInfo({required this.title,required this.address, required this.isParking, required this.latLng});
 }
 
 List<ShopInfo> testShopList = [
   ShopInfo(
     title: 'Home',
+    address: '부산광역시 남구 대연동 875-21',
+    isParking: false,
     latLng: LatLng(35.132369499264755, 129.09230978948023),
   ),
   ShopInfo(
     title: 'MasanHome',
+    address: '경상남도 창원시 마산회원구 구암남 16길 54',
+    isParking: false,
     latLng: LatLng(35.249336306317765, 128.6007908310984),
   ),
 ];
